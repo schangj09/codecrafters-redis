dd7eeb89a771a7497328a17a1ad4caf67ef713fc
Jeffrey Schang <schangj09@gmail.com>
2024-02-23 10:04:35 +0100

    stage 4 - accept multiple connections and process them in a single threaded EventLoop

---
+++ src/main/java/EventLoop.java
@@ -0,0 +1,87 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EventLoop {
+    private static final String PONG = "+PONG\r\n";
+    private static final String PING = "ping";
+
+    // keep a list of socket connections and continue checking for new connections
+    final ServerSocket serverSocket;
+    Deque<ClientConnection> clientSockets = new ConcurrentLinkedDeque<>();
+    boolean done = false;
+    ExecutorService executor;
+
+    public EventLoop(ServerSocket socket) {
+        serverSocket = socket;
+        executor = Executors.newFixedThreadPool(10); // Adjust thread count as needed
+
+        executor.execute(() -> {
+            while (true) {
+                Socket clientSocket = null;
+                try {
+                    clientSocket = serverSocket.accept();
+                    ClientConnection conn = new ClientConnection(clientSocket);
+                    clientSockets.add(conn);
+                } catch (IOException e) {
+                    System.out.println("IOException: " + e.getMessage());
+                } finally {
+                    System.out.println(String.format("Connection: %s, opened: %s", 
+                    clientSocket, clientSocket == null ? null : !clientSocket.isClosed()));
+                }
+            }
+        });
+    }
+
+    void processLoop() throws InterruptedException {
+        while (!done) {
+            // check for bytes on next socket and process
+            boolean didProcess = false;
+            Iterator<ClientConnection> iter = clientSockets.iterator();
+            for (; iter.hasNext();) {
+                ClientConnection conn = iter.next();
+                if (conn.clientSocket.isClosed()) {
+                    System.out.println(String.format("Connection closed: %s", conn.clientSocket));
+                    iter.remove();
+                    continue;
+                }
+
+                try {
+                    if (conn.inputStream.available() > 0 && !conn.clientSocket.isClosed()) {
+                        String line = conn.reader.readLine();
+                        didProcess = true;
+                        process(conn, line);
+                    }
+                } catch (IOException e) {
+                    System.out.println("IOException: " + e.getMessage());
+                }
+            }
+            // sleep a bit if there were no lines processed
+            if (!didProcess) {
+                //System.out.println("sleep 1s");
+                Thread.sleep(1000L);
+            }
+        }
+    }
+
+    void process(ClientConnection conn, String line) throws IOException {
+        System.out.println(String.format("Received line: %s", line));
+        switch (line) {
+            case PING -> {
+                conn.writer.write(PONG);
+                conn.writer.flush();
+            }
+            case "EOF" -> {
+                conn.clientSocket.close();
+            }
+            default -> {
+                // ignore command line
+            }
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,58 +1,29 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+  private static final int PORT = 6379;
+
+  public static void main(String[] args) {
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+    // Uncomment this block to pass the first stage
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
     try {
-      serverSocket = new ServerSocket(port);
+      serverSocket = new ServerSocket(PORT);
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-      BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      boolean isEof = false;
-      while (!isEof) {
-        String line = reader.readLine();
-        System.out.println(String.format("Received line: %s", line));
-        switch (line) {
-          case "ping" -> {
-            writer.write("+PONG\r\n");
-            writer.flush();
-          }
-          case "EOF" -> {
-            isEof = true;
-          }
-          default -> {
-            // ignore command line
-          }
-        }
-      }
+      System.out.println("Server started. Listening on Port " + PORT);
+      EventLoop loop = new EventLoop(serverSocket);
+      loop.processLoop();
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
+    } catch (InterruptedException e) {
+      System.out.println("InterruptedException: " + e.getMessage());
     } finally {
-      System.out.println(String.format("Connection: %s", clientSocket));
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
+      System.out.println(String.format("All connections closed"));
     }
   }
 }
