156a14e260fc71ea93a44681f0c82bf5a3029988
Jeffrey Schang <schangj09@gmail.com>
2024-03-03 13:37:50 +0100

    fix follower replication info

--- src/main/java/org/baylight/redis/FollowerReplication.java
+++ src/main/java/org/baylight/redis/FollowerReplication.java
@@ -3,8 +3,7 @@ package org.baylight.redis;
 public class FollowerReplication {
 
     public void getReplcationInfo(StringBuilder sb) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getReplcationInfo'");
+        // nothing to add for now
     }
 
 }
