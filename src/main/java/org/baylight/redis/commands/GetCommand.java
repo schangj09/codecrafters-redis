4bed0d3ad7754da2239da0d54b915836f30cd99c
Jeffrey Schang <schangj09@gmail.com>
2024-03-04 15:39:29 +0100

    use CodiumAI for GetCommand javadocs

--- src/main/java/org/baylight/redis/commands/GetCommand.java
+++ src/main/java/org/baylight/redis/commands/GetCommand.java
@@ -7,22 +7,42 @@ import org.baylight.redis.protocol.RespConstants;
 import org.baylight.redis.protocol.RespValue;
 import java.util.Map;
 
+/**
+ * Represents a GET command in a Redis server.
+ * This class is a subclass of RedisCommand and is responsible for setting the command arguments and executing the command.
+ */
 public class GetCommand extends RedisCommand {
     private RespBulkString key;
 
+    /**
+     * Constructs a new GetCommand object with the GET command type.
+     */
     public GetCommand() {
         super(Type.GET);
     }
 
+    /**
+     * Constructs a new GetCommand object with the GET command type and the specified key.
+     * @param key the key for the GET command
+     */
     public GetCommand(RespBulkString key) {
         super(Type.GET);
         this.key = key;
     }
 
-	public RespBulkString getKey() {
-		return key;
-	}
+    /**
+     * Gets the key for the GET command.
+     * @return the key for the GET command
+     */
+    public RespBulkString getKey() {
+        return key;
+    }
 
+    /**
+     * Sets the command arguments by parsing the provided RespValue array.
+     * The arguments should contain the key as the first element.
+     * @param args the command arguments
+     */
     @Override
     public void setArgs(RespValue[] args) {
         ArgReader argReader = new ArgReader(type.name(), new String[] {
@@ -33,6 +53,13 @@ public class GetCommand extends RedisCommand {
         this.key = optionsMap.get("1").asBulkString();
     }
 
+    /**
+     * Executes the GET command by retrieving the value associated with the specified key from the Redis service.
+     * If the key exists and is not expired, the value is returned as a byte array.
+     * If the key does not exist or is expired, null is returned.
+     * @param service the Redis service to execute the command on
+     * @return the value associated with the key, or null if the key does not exist or is expired
+     */
     @Override
     public byte[] execute(RedisServiceBase service) {
         if (service.containsKey(key.getValueAsString())) {
@@ -46,6 +73,10 @@ public class GetCommand extends RedisCommand {
         return RespConstants.NULL;
     }
 
+    /**
+     * Returns a string representation of the GetCommand object.
+     * @return a string representation of the GetCommand object
+     */
     @Override
     public String toString() {
         return "GetCommand [key=" + key + "]";
