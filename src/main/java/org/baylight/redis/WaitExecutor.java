b3b636f045b4aa607470e86f26ba8c101b34c793
Jeffrey Schang <schangj09@gmail.com>
2024-03-12 13:58:53 +0100

    cancel pending tasks from WaitExecutor

--- src/main/java/org/baylight/redis/WaitExecutor.java
+++ src/main/java/org/baylight/redis/WaitExecutor.java
@@ -1,6 +1,9 @@
 package org.baylight.redis;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -27,40 +30,21 @@ public class WaitExecutor {
         try {
             // send a replConf ack command to each follower on a separate thread
             // wait on the latch to block until enough acks are received
-            for (ConnectionToFollower connection : followers) {
-                asyncSendRequest(() -> {
-                    try {
-                        System.out.println(
-                                String.format("Sending replConfAck to %s", connection.toString()));
-                        System.out.println(String.format("Time %d: before send on %s",
-                                System.currentTimeMillis(), connection));
-                        connection.sendAndWaitForReplConfAck();
-                        System.out.println(String.format("Time %d: after send on %s",
-                                System.currentTimeMillis(), connection));
-                        System.out.println(Integer.toHexString(System.identityHashCode(numAcknowledged)));
-                        int prevAck = numAcknowledged.getAndIncrement();
-                        latch.countDown();
-                        System.out
-                                .println(String.format("Received replConfAck %d (prev %d) from %s",
-                                        numAcknowledged.get(), prevAck, connection.toString()));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println(String.format(
-                                "Error sending replConfAck to %s, error: %s %s, cause: %s",
-                                connection.toString(), e.getClass().getSimpleName(), e.getMessage(),
-                                e.getCause()));
-                        System.out.println(
-                                String.format("Not counted. Still waiting for %d replConfAcks.",
-                                        numToWaitFor - numAcknowledged.get()));
-                    }
+            List<Callable<Void>> followerCalls = new ArrayList<>();
+            for (ConnectionToFollower follower : followers) {
+                followerCalls.add(() -> {
+                    requestAckFromFollower(follower);
+                    return null;
                 });
             }
+            List<Future<Void>> callFutures = executorService.invokeAll(followerCalls);
+
             // extend the timeout to allow for codecrafters tests to pass - this may be needed for
             // "replication-18" which expects all replicas, even if it asks for less than all
             // of them, so we need to wait longer than the default timeout.
             long extendedTimeout = timeoutMillis + 1500L;
             long before = System.currentTimeMillis();
-            System.out.println(String.format("Time %d: waiting for %d millis for acks.", before,
+            System.out.println(String.format("Time %d: waiting u to %d millis for acks.", before,
                     extendedTimeout));
             asyncSendRequest(() -> {
                 try {
@@ -69,7 +53,8 @@ public class WaitExecutor {
                         System.out.println(String.format(
                                 "Timed out waiting for %d replConfAcks. Received %d acks.",
                                 numToWaitFor, numAcknowledged.get()));
-                        System.out.println(Integer.toHexString(System.identityHashCode(numAcknowledged)));
+                        System.out.println(
+                                Integer.toHexString(System.identityHashCode(numAcknowledged)));
                         // sleep for the extended timeout
                         System.out.println(String.format(
                                 "Time %d: sleeping extend for %d millis for acks.",
@@ -77,11 +62,11 @@ public class WaitExecutor {
                         // give extended time for codecrafters tests to pass
                         waitUntil = before + extendedTimeout;
                     } else {
-                        // latch returned true, so we have received requested number, but still let's wait
-                        // the full timeout duration for codecrafters tests to pass
+                        // latch returned true, so we have received requested number, but still
+                        // let's wait the full timeout duration for codecrafters tests to pass
                         waitUntil = before + timeoutMillis;
                     }
-                    // although we timed out or have enough acks to return now, we will sleep 
+                    // although we timed out or have enough acks to return now, we will sleep
                     // a little longer until we get ack from all followers
                     // - this is for codecrafters test "replication-17" to pass since it
                     // expects us to return the count of all follower replicas
@@ -101,6 +86,16 @@ public class WaitExecutor {
             long after = System.currentTimeMillis();
             System.out.println(String.format("Time %d: after extended task wait, elapsed time: %d.",
                     after, after - before));
+
+            // cancel all pending tasks
+            for (Future<Void> future : callFutures) {
+                int countCancelled = 0;
+                if (future.cancel(true)) {
+                    countCancelled++;
+                }
+                System.out.println(String.format("Cancelled %d of %d tasks.", countCancelled,
+                        callFutures.size()));
+            }
         } catch (Exception e) {
             System.out
                     .println(String.format("Error while sending %d replConfAcks. Received %d acks.",
@@ -110,6 +105,30 @@ public class WaitExecutor {
         System.out.println(String.format("Returning %d of %d requested acks.",
                 numAcknowledged.get(), numToWaitFor));
         return numAcknowledged.get();
+    }
+
+    private void requestAckFromFollower(ConnectionToFollower connection) {
+        try {
+            System.out.println(String.format("Sending replConfAck to %s", connection.toString()));
+            System.out.println(String.format("Time %d: before send on %s",
+                    System.currentTimeMillis(), connection));
+            connection.sendAndWaitForReplConfAck();
+            System.out.println(String.format("Time %d: after send on %s",
+                    System.currentTimeMillis(), connection));
+            System.out.println(Integer.toHexString(System.identityHashCode(numAcknowledged)));
+            int prevAck = numAcknowledged.getAndIncrement();
+            latch.countDown();
+            System.out.println(String.format("Received replConfAck %d (prev %d) from %s",
+                    numAcknowledged.get(), prevAck, connection.toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(
+                    String.format("Error sending replConfAck to %s, error: %s %s, cause: %s",
+                            connection.toString(), e.getClass().getSimpleName(), e.getMessage(),
+                            e.getCause()));
+            System.out.println(String.format("Not counted. Still waiting for %d replConfAcks.",
+                    numToWaitFor - numAcknowledged.get()));
+        }
     }
 
     private Future<?> asyncSendRequest(Runnable runnable) {
