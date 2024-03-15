c1ff38b0b36772218b927f5085bc5e77b6549731
Jeffrey Schang <schangj09@gmail.com>
2024-03-15 19:57:19 +0100

    add command line options for dir and dbfilename

--- src/main/java/org/baylight/redis/RedisServiceOptions.java
+++ src/main/java/org/baylight/redis/RedisServiceOptions.java
@@ -1,5 +1,7 @@
 package org.baylight.redis;
 
+import java.io.File;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -13,27 +15,36 @@ public class RedisServiceOptions {
     private String role = RedisConstants.LEADER;
     private String replicaof = null;
     private int replicaofPort = RedisConstants.DEFAULT_PORT;
+    private String dir = ".";
+    private String dbfilename = "redis.rdb";
 
     public boolean parseArgs(String[] args) {
         // Define the options
         Options options = new Options();
 
-        Option portOption = Option.builder()
-                .longOpt("port")
-                .hasArg(true)
-                .desc("The port number to use")
-                .required(false) // Make this option optional
-                .type(Number.class)
-                .build();
+        Option portOption = Option.builder().longOpt("port").hasArg(true)
+                .desc("The port number to use").required(false) // Make this option optional
+                .type(Number.class).build();
         options.addOption(portOption);
 
-        Option replicaofOption = Option.builder()
-                .longOpt("replicaof")
-                .numberOfArgs(2)
-                .desc("The host and port of the replica")
-                .required(false) // Make this option optional
+        Option replicaofOption = Option.builder().longOpt("replicaof").numberOfArgs(2)
+                .desc("The host and port of the replica").required(false) // Make this option
+                                                                          // optional
                 .build();
         options.addOption(replicaofOption);
+
+        Option dirOption = Option.builder().longOpt("dir").hasArg(true)
+                .desc("The directory where RDB files are stored").required(false) // Make this
+                                                                                  // option optional
+                .build();
+        options.addOption(dirOption);
+
+        Option dbfilenameOption = Option.builder().longOpt("dbfilename").hasArg(true)
+                .valueSeparator(' ').desc("The name of the RDB file").required(false) // Make this
+                                                                                      // option
+                                                                                      // optional
+                .build();
+        options.addOption(dbfilenameOption);
 
         // Create a parser and parse the command line arguments
         CommandLineParser parser = new DefaultParser();
@@ -55,12 +66,35 @@ public class RedisServiceOptions {
                 String[] replicaofStrings = cmd.getOptionValues("replicaof");
                 replicaof = replicaofStrings[0];
                 replicaofPort = Integer.parseInt(replicaofStrings[1]);
-                System.out.println("Replicaof specified: " + getReplicaof() + " " + getReplicaofPort());
+                System.out.println(
+                        "Replicaof specified: " + getReplicaof() + " " + getReplicaofPort());
                 if (replicaofPort <= 0 || replicaofPort > 65535) {
-                    throw new ParseException("Port must be less than or equal to 65535: " + replicaofPort);
+                    throw new ParseException(
+                            "Port must be less than or equal to 65535: " + replicaofPort);
                 }
 
                 role = RedisConstants.FOLLOWER;
+            }
+
+            if (cmd.hasOption("dir")) {
+                dir = cmd.getOptionValue("dir");
+                System.out.println("Dir specified: " + getDir());
+                // check if dir is a valid directory
+                File dirFile = new File(getDir());
+                if (!dirFile.isDirectory()) {
+                    throw new ParseException("Invalid directory: " + getDir());
+                }
+            }
+
+            if (cmd.hasOption("dbfilename")) {
+                dbfilename = cmd.getOptionValue("dbfilename");
+                System.out.println("Dbfilename specified: " + getDbfilename());
+                // check if dbfilename is a valid file
+                File db = new File(getDir(), getDbfilename());
+                if (!db.isFile()) {
+                    throw new ParseException(
+                            "Invalid dbfilename in dir: " + getDir() + "/" + getDbfilename());
+                }
             }
 
         } catch (ParseException e) {
@@ -72,6 +106,17 @@ public class RedisServiceOptions {
             return false;
         }
         return true;
+    }
+
+    public String getConfigValue(String config) {
+        return switch (config) {
+        case "port" -> String.valueOf(port);
+        case "role" -> role;
+        case "replicaof" -> replicaof + " " + replicaofPort;
+        case "dir" -> dir;
+        case "dbfilename" -> dbfilename;
+        default -> null;
+        };
     }
 
     public int getPort() {
@@ -90,4 +135,13 @@ public class RedisServiceOptions {
     public int getReplicaofPort() {
         return replicaofPort;
     }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public String getDbfilename() {
+        return dbfilename;
+    }
+
 }
